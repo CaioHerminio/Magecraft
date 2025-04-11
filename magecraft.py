c8d314ad2064e07e13d00fd4897d7d96dfdaac71
Caio Hermínio <114634602+CaioHerminio@users.noreply.github.com>
2025-04-11 15:01:51 -0300

    Create magecraft.py

---
+++ magecraft.py
@@ -0,0 +1,9 @@
+# 🧙 Magecraft University - Python Edition
+
+color = input("Enter a color: ")
+animal = input("Enter an animal: ")
+mana = input("Enter a numhber between 100 and 900: ")
+
+print("\nWelcome to Magecraft University")
+print(f"Your alias is The {color} {animal}")
+print(f"Your current mana level is {mana}, but we will polish you so that you reach your maximum potential!")
