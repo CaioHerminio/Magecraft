52ee3ef8c2c526356ea9a32ce6740f46cd5bcc4b
Caio Hermínio <114634602+CaioHerminio@users.noreply.github.com>
2026-07-01 17:15:52 -0300

    Add Magecraft University introduction program

---
+++ main.py
@@ -0,0 +1,9 @@
+# 🧙 Magecraft University - Python Edition
+
+color = input("Enter a color: ")
+animal = input("Enter an animal: ")
+mana = int(input("Enter a number between 100 and 900: "))
+
+print("\nWelcome to Magecraft University")
+print(f"Your alias is The {color} {animal}")
+print(f"Your current mana level is {mana}, but we will polish you so that you reach your maximum potential!")
